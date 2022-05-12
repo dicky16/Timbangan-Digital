4636259d0b376935a995595d783d7ba83c464a96
Dikky Setiawan <setyawandicky88@gmail.com>
2022-05-12 20:12:27 +0700

    landing page

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable new-cap */
+const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
 const m = require('../middleware/cek.login')
@@ -7,6 +8,9 @@ router.use('/api', require('./api'));
 router.use('/auth', require('./auth'));
 
 //home route
+router.use('/', function(req, res) {
+    res.render('landing/index')
+})
 router.use('/home', m.cekLogin,  require('./home'))
 router.use('/', m.cekLogin, require('./profile'))
 router.use('/', m.cekLogin,  require('./atasan'))
