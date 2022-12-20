4329551c927571962967bf58fb20160364ae50cc
dikky setiwan <setyawandicky88@gmail.com>
2022-12-21 01:14:44 +0700

    change rumput

--- routes/api/all/index.js
+++ routes/api/all/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const c = require('./all.controller')
 
-router.get('/jenis-rumput', c.findAllJenisRumput)
-router.get('/asal-rumput', c.findAllAsalRumput)
+router.get('/jenis', c.findAllJenisRumput)
+router.get('/asal', c.findAllAsalRumput)
 router.get('/truk', c.findAllTruk)
 
 module.exports = router
